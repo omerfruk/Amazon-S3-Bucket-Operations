f27e0b93619b0703f506da5336b18b3a4797e060
omerfruk <omer.fruk3547@gmail.com>
2022-05-22 04:43:17 +0300

    New : Buckets işlemleri değiştiriliyor

--- main.go
+++ main.go
@@ -2,31 +2,50 @@ package main
 
 import (
 	"fmt"
-	"net/url"
+	"github.com/gofiber/fiber/v2"
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
 const (
-	BUCKET_NAME = "omerfruk"
-	REGION      = "us-west-2"
+	REGION             string = "eu-central-1"
+	S3CREDENTIALID     string = "AKIAYU3HHMJTEBFC2IZC"
+	S3CREDENTIALSECRET string = "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"
+	BASEDIR            string = "omerfruk-buclets"
 )
 
-func main() {
+var Sess = session.Session{}
 
+func main() {
+	app := fiber.New()
+
+	sess, err := session.NewSession(
+		&aws.Config{
+			Region: aws.String(REGION),
+			Credentials: credentials.NewStaticCredentials(
+				S3CREDENTIALID,
+				S3CREDENTIALSECRET,
+				"",
+			),
+		})
+	if err != nil {
+		panic(err)
+	}
+
+	Sess = *sess
+
+	app.Get("/buckets", ListBuckets)
+	app.Get("/folders", ListFolder)
+
+	app.Listen(":3000")
 }
 
-func ListBuckets() {
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String(BUCKET_NAME)},
-	)
-
-	// Create S3 service client
-	svc := s3.New(sess)
+func ListBuckets(c *fiber.Ctx) error {
+	svc := s3.New(&Sess)
 
 	result, err := svc.ListBuckets(nil)
 	if err != nil {
@@ -40,247 +59,11 @@ func ListBuckets() {
 			aws.StringValue(b.Name), aws.TimeValue(b.CreationDate))
 	}
 
+	return c.JSON(result.Buckets)
 }
 
-func CreateBuckets() {
-	if len(os.Args) != 2 {
-		exitErrorf("Bucket name missing!\nUsage: %s bucket_name", os.Args[0])
-	}
-
-	bucket := os.Args[1]
-
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String(BUCKET_NAME)},
-	)
-
-	// Create S3 service client
-	svc := s3.New(sess)
-
-	_, err = svc.CreateBucket(&s3.CreateBucketInput{
-		Bucket:                    aws.String(bucket),
-		CreateBucketConfiguration: &s3.CreateBucketConfiguration{LocationConstraint: aws.String(REGION)},
-	})
-	if err != nil {
-		exitErrorf("Unable to create bucket %q, %v", bucket, err)
-	}
-
-	// Wait until bucket is created before finishing
-	fmt.Printf("Waiting for bucket %q to be created...\n", bucket)
-
-	err = svc.WaitUntilBucketExists(&s3.HeadBucketInput{
-		Bucket: aws.String(bucket),
-	})
-
-	if err != nil {
-		exitErrorf("Error occurred while waiting for bucket to be created, %v", bucket)
-	}
-
-	fmt.Printf("Bucket %q successfully created\n", bucket)
-
-}
-
-func ListBucketsItems() {
-	if len(os.Args) != 2 {
-		exitErrorf("Bucket name required\nUsage: %s bucket_name",
-			os.Args[0])
-	}
-
-	bucket := os.Args[1]
-
-	// Initialize a session in us-west-2 that the SDK will use to load
-	// credentials from the shared credentials file ~/.aws/credentials.
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-west-2")},
-	)
-
-	// Create S3 service client
-	svc := s3.New(sess)
-
-	// Get the list of items
-	resp, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: aws.String(bucket)})
-	if err != nil {
-		exitErrorf("Unable to list items in bucket %q, %v", bucket, err)
-	}
-
-	for _, item := range resp.Contents {
-		fmt.Println("Name:         ", *item.Key)
-		fmt.Println("Last modified:", *item.LastModified)
-		fmt.Println("Size:         ", *item.Size)
-		fmt.Println("Storage class:", *item.StorageClass)
-		fmt.Println("")
-	}
-
-	fmt.Println("Found", len(resp.Contents), "items in bucket", bucket)
-	fmt.Println("")
-}
-
-func UploadFileToBucket() {
-	if len(os.Args) != 3 {
-		exitErrorf("bucket and file name required\nUsage: %s bucket_name filename",
-			os.Args[0])
-	}
-
-	bucket := os.Args[1]
-	filename := os.Args[2]
-
-	file, err := os.Open(filename)
-	if err != nil {
-		exitErrorf("Unable to open file %q, %v", err)
-	}
-
-	defer file.Close()
-
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-west-2")},
-	)
-
-	uploader := s3manager.NewUploader(sess)
-
-	_, err = uploader.Upload(&s3manager.UploadInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(filename),
-		Body:   file,
-	})
-	if err != nil {
-		// Print the error and exit.
-		exitErrorf("Unable to upload %q to %q, %v", filename, bucket, err)
-	}
-
-	fmt.Printf("Successfully uploaded %q to %q\n", filename, bucket)
-}
-
-func DownloadFileToBucket() {
-
-	if len(os.Args) != 3 {
-		exitErrorf("Bucket and item names required\nUsage: %s bucket_name item_name",
-			os.Args[0])
-	}
-
-	bucket := os.Args[1]
-	item := os.Args[2]
-
-	file, err := os.Create(item)
-	if err != nil {
-		exitErrorf("Unable to open file %q, %v", item, err)
-	}
-
-	defer file.Close()
-
-	sess, _ := session.NewSession(&aws.Config{
-		Region: aws.String("us-west-2")},
-	)
-
-	downloader := s3manager.NewDownloader(sess)
-
-	numBytes, err := downloader.Download(file,
-		&s3.GetObjectInput{
-			Bucket: aws.String(bucket),
-			Key:    aws.String(item),
-		})
-	if err != nil {
-		exitErrorf("Unable to download item %q, %v", item, err)
-	}
-
-	fmt.Println("Downloaded", file.Name(), numBytes, "bytes")
-}
-
-func CopyItemFromBucketToAnother() {
-
-	if len(os.Args) != 4 {
-		exitErrorf("Bucket, item, and other bucket names required\nUsage: go run s3_copy_object bucket item other-bucket")
-	}
-
-	bucket := os.Args[1]
-	item := os.Args[2]
-	other := os.Args[3]
-
-	source := bucket + "/" + item
-
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-west-2")},
-	)
-
-	// Create S3 service client
-	svc := s3.New(sess)
-
-	// Copy the item
-	_, err = svc.CopyObject(&s3.CopyObjectInput{Bucket: aws.String(other),
-		CopySource: aws.String(url.PathEscape(source)), Key: aws.String(item)})
-	if err != nil {
-		exitErrorf("Unable to copy item from bucket %q to bucket %q, %v", bucket, other, err)
-	}
-
-	// Wait to see if the item got copied
-	err = svc.WaitUntilObjectExists(&s3.HeadObjectInput{Bucket: aws.String(other), Key: aws.String(item)})
-	if err != nil {
-		exitErrorf("Error occurred while waiting for item %q to be copied to bucket %q, %v", bucket, item, other, err)
-	}
-
-	fmt.Printf("Item %q successfully copied from bucket %q to bucket %q\n", item, bucket, other)
-
-}
-
-func DeleteItemInBucket() {
-	if len(os.Args) != 3 {
-		exitErrorf("Bucket and object name required\nUsage: %s bucket_name object_name",
-			os.Args[0])
-	}
-
-	bucket := os.Args[1]
-	obj := os.Args[2]
-
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-west-2")},
-	)
-
-	// Create S3 service client
-	svc := s3.New(sess)
-
-	_, err = svc.DeleteObject(&s3.DeleteObjectInput{Bucket: aws.String(bucket), Key: aws.String(obj)})
-	if err != nil {
-		exitErrorf("Unable to delete object %q from bucket %q, %v", obj, bucket, err)
-	}
-
-	err = svc.WaitUntilObjectNotExists(&s3.HeadObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(obj),
-	})
-	fmt.Printf("Object %q successfully deleted\n", obj)
-}
-
-func DeleteBucket() {
-	if len(os.Args) != 2 {
-		exitErrorf("bucket name required\nUsage: %s bucket_name", os.Args[0])
-	}
-
-	bucket := os.Args[1]
-
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-west-2")},
-	)
-
-	// Create S3 service client
-	svc := s3.New(sess)
-
-	_, err = svc.DeleteBucket(&s3.DeleteBucketInput{
-		Bucket: aws.String(bucket),
-	})
-	if err != nil {
-		exitErrorf("Unable to delete bucket %q, %v", bucket, err)
-	}
-
-	// Wait until bucket is deleted before finishing
-	fmt.Printf("Waiting for bucket %q to be deleted...\n", bucket)
-
-	err = svc.WaitUntilBucketNotExists(&s3.HeadBucketInput{
-		Bucket: aws.String(bucket),
-	})
-
-	if err != nil {
-		exitErrorf("Error occurred while waiting for bucket to be deleted, %v", bucket)
-	}
-
-	fmt.Printf("Bucket %q successfully deleted\n", bucket)
+func ListFolder(c *fiber.Ctx) error {
+	return c.JSON(nil)
 }
 
 func exitErrorf(msg string, args ...interface{}) {
